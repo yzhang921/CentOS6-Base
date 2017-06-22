a0a1dd800a5ebaadb52e79a4db6fe6d457fae211
yzhang921 <peter.zhang921@gmail.com>
2017-06-22 09:50:51 +0800

    update myid generate method

--- resource/conf-zookeeper/start-zk-daemons.sh
+++ resource/conf-zookeeper/start-zk-daemons.sh
@@ -2,5 +2,5 @@
 curDir=$(cd `dirname $0`; pwd)
 cd $curDir
 
-pssh -h zk-nodes -i "echo `hostname | grep -oP '\d'`  >  /var/lib/zookeeper/myid"
+pssh -h zk-nodes -i "hostname | grep -oP '\d' > /var/lib/zookeeper/myid"
 pssh -h zk-nodes -i "service zookeeper-server start"
