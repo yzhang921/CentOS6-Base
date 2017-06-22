ec72a439f0f95ad6e077fac0bf4f57f669aa63e6
yzhang921 <peter.zhang921@gmail.com>
2017-06-22 09:40:18 +0800

    remove zk initial service

--- resource/conf-zookeeper/start-zk-daemons.sh
+++ resource/conf-zookeeper/start-zk-daemons.sh
@@ -2,4 +2,5 @@
 curDir=$(cd `dirname $0`; pwd)
 cd $curDir
 
-pssh -h zk-nodes -i "echo `hostname | grep -oP '\d'`  >  /var/lib/zookeeper/myid; service zookeeper-server init; service zookeeper-server start"
+pssh -h zk-nodes -i "echo `hostname | grep -oP '\d'`  >  /var/lib/zookeeper/myid"
+pssh -h zk-nodes -i "service zookeeper-server start"
