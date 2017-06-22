5f37ce4c3e770a2c491b68d0d4e59a9e2dcd0ebc
yzhang921 <peter.zhang921@gmail.com>
2017-06-22 10:48:31 +0800

    start-zk-daemons accept init param

--- resource/conf-zookeeper/start-zk-daemons.sh
+++ resource/conf-zookeeper/start-zk-daemons.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+init=$1
+
 curDir=$(cd `dirname $0`; pwd)
 cd $curDir
 
-pssh -v -h zk-nodes -i "conf-zookeeper/start-zk.sh"
+pssh -v -h zk-nodes -i "conf-zookeeper/start-zk.sh ${init}"
--- resource/conf-zookeeper/start-zk.sh
+++ resource/conf-zookeeper/start-zk.sh
@@ -4,10 +4,16 @@
 curDir=$(cd `dirname $0`; pwd)
 cd $curDir
 
+init=$1
 
 myid=`hostname | grep -oP '\d'`
 rm -rf /var/lib/zookeeper/*
 echo ${myid} > /var/lib/zookeeper/myid
 cat /var/lib/zookeeper/myid
-service zookeeper-server init --myid=${myid}
+
+if [ "$init" = "init" ]; then
+  echo "[INFO] Start intitail ZK cluster"
+  service zookeeper-server init --myid=${myid}
+fi
+
 service zookeeper-server start
