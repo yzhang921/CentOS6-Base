2f6a2de12e9926b182a553de2f6f3879ccf64106
yzhang921 <peter.zhang921@gmail.com>
2017-06-22 09:22:09 +0800

    update start zk shell

--- resource/conf-zookeeper/start-zk-daemons.sh
+++ resource/conf-zookeeper/start-zk-daemons.sh
@@ -2,4 +2,4 @@
 curDir=$(cd `dirname $0`; pwd)
 cd $curDir
 
-pssh -h zk-nodes -i "hostname | grep -oP '\d' > /var/lib/zookeeper/myid; service zookeeper-server init; service zookeeper-server start"
+pssh -h zk-nodes -i "echo `hostname | grep -oP '\d'`  >  /var/lib/zookeeper/myid; service zookeeper-server init; service zookeeper-server start"
