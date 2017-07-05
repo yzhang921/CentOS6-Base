8c2cebd2b3371840e21e4117d78f1a2115b402b3
yzhang921 <peter.zhang921@gmail.com>
2017-07-05 08:53:25 +0800

    fix bug  get localip shell script

--- resource/conf-elk/init-es.sh
+++ resource/conf-elk/init-es.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-local_ip=ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " "
+local_ip=`ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " "`
 echo "network.host: ${local_ip}" >> /etc/elasticsearch/elasticsearch.yml
