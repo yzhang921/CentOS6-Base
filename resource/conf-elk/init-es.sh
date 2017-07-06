4aaa3412ce859487ce385918ec74deec3841658c
yzhang921 <peter.zhang921@gmail.com>
2017-07-06 08:53:23 +0800

    configure kibana
    change coordinator network.host to ip

--- resource/conf-elk/init-es.sh
+++ resource/conf-elk/init-es.sh
@@ -3,14 +3,9 @@
 node=$1
 
 local_ip=`ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " "`
+hostname=`hostname`
 
-if [ "$node" = "master" ]; then
-  cp -fR /root/conf-elk/es-coordinator.yml /etc/elasticsearch/elasticsearch.yml
-  echo "transport.host: ${local_ip}" >> /etc/elasticsearch/elasticsearch.yml
-else
-  echo "network.host: ${local_ip}" >> /etc/elasticsearch/elasticsearch.yml
-fi
-
+echo "network.host: ${local_ip}" >> /etc/elasticsearch/elasticsearch.yml
 
 # [ERROR][o.e.b.Bootstrap          ] [es-master] node validation exception
 # [1] bootstrap checks failed
@@ -21,3 +16,24 @@ root                 hard    nproc           2048
 elasticsearch        soft    nproc           2048
 relasticsearch       hard    nproc           2048
 " >> /etc/security/limits.conf
+
+
+# ####################################
+#
+# Coordinator configuration
+#
+
+
+coord_config="
+node.master: false
+node.data: false
+node.ingest: false
+transport.host: ${local_ip}
+"
+
+if [ "${hostname}" = "es-master" ]; then
+  echo "${coord_config}" >> /etc/elasticsearch/elasticsearch.yml
+fi
+
+
+
