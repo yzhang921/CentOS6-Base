0cda198b220a2fe3536d99d8475a4ddb4aeb5efb
yzhang921 <peter.zhang921@gmail.com>
2017-07-10 17:54:18 +0800

    copy cdh conf to druid folder context

--- druid.io/start-druid-containers.sh
+++ druid.io/start-druid-containers.sh
@@ -2,3 +2,88 @@
 
 curDir=$(cd `dirname $0`; pwd)
 cd $curDir
+
+function display_usage() {
+cat << EOF
+  usage: $0 -m|-mode=[run|start|stop] -i|-image=[image name] -h|--hlep
+    -mode   -m   run mode
+    -image  -i   image name, default name is "run-master"
+EOF
+}
+
+
+if [ "$#" = 0 ]; then
+    display_usage
+fi
+
+echo "[INFO] Parse all the arguments...."
+while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
+  key=${1%%=*}  # 从后往前删掉最大匹配的字符得到key
+  value=${1#*=} # 从前往后删掉最小匹配的字符得到value
+  case "${key}" in
+    -h | --help)
+       display_usage
+       exit 1
+       ;;
+    -m | -mode)
+        mode=${value}
+        echo "set [mode] to ${value}"
+        ;;
+    -i | -image)
+        image=${value}
+        echo "set [image] to ${value}"
+        ;;
+    * )
+        echo "[ERROR]: $key is an unknown parameter."
+        display_usage
+        exit 1 ;;
+  esac
+  shift       # Check next set of parameters.
+done
+
+if [ "$image" = "" ]; then
+   image="centos6-druid"
+fi
+
+if [ "$mode" = "" ]; then
+   display_usage
+   exit 1
+fi
+
+
+function checkAndStart() {
+  c_name=$1
+  container_id=`docker ps -a -q --filter "name=${c_name}"`
+  if [ "$container_id" != "" ]; then
+    echo "[INFO] Find container ${c_name} with id ${container_id}, start..."
+    sudo docker start ${container_id}
+  else
+    echo "[WARN] NO container with ${c_name}, try to run new one..."
+    $2
+  fi
+}
+
+# ======================================================================================================================================
+# start hadoop master container
+MASTER_NAME="druid-coord"
+
+function run-master() {
+  sudo docker rm -f ${MASTER_NAME} &> /dev/null
+  echo "[INFO] Run ${MASTER_NAME} container..."
+  sudo docker run --name=${MASTER_NAME} \
+      -itd \
+      --hostname=${MASTER_NAME} \
+      --network=hadoop \
+      -p 18081:8081  \
+      ${image}
+  # 8081: Coordinator
+}
+
+if [ "$mode" = "run" ]; then
+  run-master
+elif [ "$mode" = "start" ]; then
+  checkAndStart ${MASTER_NAME} "run-master"
+elif [ "$mode" = "stop" ]; then
+  echo "[INFO] stop ${MASTER_NAME} container..."
+  sudo docker stop ${MASTER_NAME}
+fi
