881f5f69574a4fd480986d277201fa2bfe1a468e
yzhang921 <peter.zhang921@gmail.com>
2017-07-11 11:34:25 +0800

    add start log

--- druid.io/resource/conf-druid/daemons-druid.sh
+++ druid.io/resource/conf-druid/daemons-druid.sh
@@ -4,16 +4,18 @@ curDir=$(cd `dirname $0`; pwd)
 cd $DRUID_HOME
 
 
-# start coordinator
+echo "[INFO] Start coordinator..."
 ../druid-sbin/start-coordinator.sh
-# start overload
+echo "[INFO] Start overload..."
 ../druid-sbin/start-overload.sh
 
 
 # start historicals and middleManagers
+echo "[INFO] Start historicals..."
 pssh -h ../druid-sbin/historicals -i "/root/druid-sbin/start-historical.sh" | grep -v "Permanently added"
+echo "[INFO] Start middleManagers..."
 pssh -h ../druid-sbin/historicals -i "/root/druid-sbin/start-middleManager.sh" | grep -v "Permanently added"
 
 
-# start broker
+echo "[INFO] Start broker..."
 ssh root@druid-broker /root/druid-sbin/start-broker.sh
